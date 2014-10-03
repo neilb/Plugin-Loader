b9532a76eb6e027b9766f3e6f262967335f34c28
Neil Bowers <neil@bowers.com>
2014-10-03 23:05:16 +0100

    Added max_depth attribute, to control search depth
    By default this isn't set, which means find_modules() will return
    all modules within the namespace. Setting it to 1 will only
    return modules that are "immediately within" the namespace.

--- lib/Plugin/Loader.pm
+++ lib/Plugin/Loader.pm
@@ -6,6 +6,8 @@ use File::Spec::Functions qw/ catfile splitdir /;
 use Path::Iterator::Rule;
 use Carp qw/ croak /;
 
+has 'max_depth' => (is => 'rw');
+
 sub find_modules
 {
     my ($self, $base) = @_;
@@ -19,6 +21,7 @@ sub find_modules
 
         my $rule = Path::Iterator::Rule->new;
         $rule->perl_module;
+        $rule->max_depth($self->max_depth) if $self->max_depth;
         foreach my $file ($rule->all($path)) {
             my $modpath = $file;
 
@@ -73,6 +76,26 @@ This module was inspired by L<Mojo::Loader>, which I have used in
 a number of projects. But some people were wary of requiring L<Mojolicious>
 just to get a module loader, which prompted me to create C<Plugin::Loader>.
 
+=head2 max_depth
+
+C<Plugin::Loader> has an optional C<max_depth> attribute.
+If you set this to 1, then C<find_modules> will only report modules
+that are immediately within the namespace specified.
+
+Let's say you have all of the CPAN plugins for the template toolkit
+installed locally. If you don't specify C<max_depth>, then C<find_modules>
+would return L<Template::Plugin::Filter::Minify::JavaScript>
+as well as L<Template::Plugin::File>. If you set C<max_depth> to 1,
+then you'd get the latter but not the former.
+
+Why might you want to do that?
+
+You might have a convention where plugins are the modules immediately
+within the specified namespace, but that each plugin can have additional
+modules within its own namespace.
+
+So typically you'll either not set C<max_depth>, or you'll set it to 1.
+
 =head1 METHODS
 
 =head2 find_modules
@@ -82,9 +105,8 @@ that were found in C<@INC>. For example:
 
  @plugins = $loader->find_modules('Template::Plugin');
 
-This will find all modules in the given namespace, regardless of depth.
-For example, it will find L<Template::Plugin::File>
-and L<Template::Plugin::Filter::Minify::JavaScript>.
+By default this will find all modules in the given namespace,
+unless you've specified a maximum search depth, as described above.
 
 =head2 load_module
 
@@ -93,7 +115,11 @@ If loading fails, then we C<croak>.
 
 =head1 SEE ALSO
 
-L<Mojo::Loader>, L<all>, L<lib::require::all>, L<MAD::Loader>,
+L<Mojo::Loader> was the inspiration for this module, but has
+a slightly different interface. In particular, it has C<max_depth>
+hard-coded to 1.
+
+L<all>, L<lib::require::all>, L<MAD::Loader>,
 L<Module::Find>, L<Module::Recursive::Require>, L<Module::Require>.
 
 =head1 REPOSITORY
